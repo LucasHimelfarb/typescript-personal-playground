db2c6f8305a18fa92e72aeace2e6d8e32c2b85e8
Lucas Himelfarb <46998551+LucasHimelfarb@users.noreply.github.com>
2022-07-16 19:52:52 -0300

    Add challenge_1

---
+++ src/challenges/challenge_1/challenge_1.ts
@@ -0,0 +1,15 @@
+
+"use strict"
+
+const sum = (a: number, b: number) => {
+  let _a = a;
+  let _b = b;
+
+  const result = _a + _b;
+
+  return result;
+}
+
+const sum1 = sum(1, 1);
+const sum2 = sum(2, 3);
+const sum3 = sum(5, 5);
