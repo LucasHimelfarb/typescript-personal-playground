836332850e5ba1d701215d11c4652db5f9df4dd6
Lucas Himelfarb <46998551+LucasHimelfarb@users.noreply.github.com>
2022-07-16 19:01:34 -0300

    Add activity_4

---
+++ playground/activity_4.ts
@@ -0,0 +1,19 @@
+
+"use strict"
+
+interface Figure {
+  base: number;
+  height: number;
+}
+
+class GeometricFigure {
+  base: number;
+  height: number;
+
+  constructor(base: number, height: number) {
+    this.base = base;
+    this.height = height
+  }
+}
+
+const GeometricFigure_1: Figure = new GeometricFigure(20, 20);
