1df2e5213a64872fcb35cdea20bae192d83317a1
Lucas Himelfarb <46998551+LucasHimelfarb@users.noreply.github.com>
2022-07-16 17:55:36 -0300

    Add activity_2

---
+++ playground/activity_2.ts
@@ -0,0 +1,12 @@
+
+"use strict";
+
+const movie: {
+  movieName: string,
+  movieDate: Date,
+  movieTags: string[]
+} = {
+  movieName: 'Sample movie',
+  movieDate: new Date(),
+  movieTags: ['comedy', 'action', 'terror']
+};
