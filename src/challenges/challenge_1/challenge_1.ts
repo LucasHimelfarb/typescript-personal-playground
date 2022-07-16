2af9bbd279119cd0774603973b30bd2c9ea1a977
Lucas Himelfarb <46998551+LucasHimelfarb@users.noreply.github.com>
2022-07-16 20:04:03 -0300

    Delete unnecessary lines in challenge_1

--- src/challenges/challenge_1/challenge_1.ts
+++ src/challenges/challenge_1/challenge_1.ts
@@ -6,12 +6,7 @@
 // Return the result
 
 const sum = (a: number, b: number) => {
-  let _a = a;
-  let _b = b;
-
-  const result = _a + _b;
-
-  return result;
+  return a + b;
 }
 
 const sum1 = sum(1, 1);
