c8c67dcb5b7e7ff21acbe088e5149c98fda96713
Lucas Himelfarb <46998551+LucasHimelfarb@users.noreply.github.com>
2022-07-17 00:23:59 -0300

    Add challenge_4

---
+++ src/challenges/challenge_4/challenge_4.ts
@@ -0,0 +1,11 @@
+
+"use strict"
+
+// Write a function that takes a value as argument
+// Return the type of the value
+
+const getTypeOfValue = (value: any): string => {
+  const result = typeof value;
+
+  return result;
+}
